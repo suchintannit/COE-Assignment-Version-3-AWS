3e208631c2e23593574130394926fbe687b35d19
Suchintan Mishra <suchintan.mishra@gmail.com>
2022-08-07 10:52:49 +0530

    final commit

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
   sudo apt-get update -y
   echo "Installing Docker#####################################################\n"
   sudo apt-get install -y docker.io
-  echo "Adding the Kubernetes Repo#############################################\n"
+  echo "Adding the Kubernetes Repo##############################################\n"
   curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
   sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
   sudo apt-get update -y
